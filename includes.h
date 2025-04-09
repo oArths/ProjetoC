108635127b354741517a3beea722885040b36120
Arthur <arthurferreia1417@gmail.com>
2025-04-09 20:29:08 -0300

    FIX: corrigindo erros de dois enters para enviar e de dado sendo jogado para a proximo variavel

--- includes.h
+++ includes.h
@@ -6,21 +6,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_TITULO 20
+#define MAX_TEXTO 200
+#define MAX_TAG 20
 typedef struct Tags {
-    char tag[3][20];
-  } Tags;
-  
-  typedef struct {
-    int id;
-    char titulo[20];
-    char texto[200];
-    struct Tags tags;
-  } listaNotas;
-  
-  typedef struct User {
-    int id;
-    char nome[40];
-    char senha[8];
-} User;
+  char tag[3][MAX_TAG + 1];
+} Tags;
 
+typedef struct {
+  int id;
+  char titulo[MAX_TITULO + 1];
+  char texto[MAX_TEXTO + 1];
+  struct Tags tags;
+} listaNotas;
+
+typedef struct User {
+  int id;
+  char nome[40];
+  char senha[8];
+} User;
 #endif  
